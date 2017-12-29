9b93b3fb56a3dde1a88f962bd4658d19f520ee2e
Mikael Ståldal <mikael@staldal.nu>
2017-12-29 12:19:53 +0100

    LOG4J2-2163 Use FilteredObjectInputStream from log4j-api

--- log4j-server/src/main/java/org/apache/logging/log4j/server/ObjectInputStreamLogEventBridge.java
+++ log4j-server/src/main/java/org/apache/logging/log4j/server/ObjectInputStreamLogEventBridge.java
@@ -24,6 +24,7 @@ import java.util.List;
 
 import org.apache.logging.log4j.core.LogEvent;
 import org.apache.logging.log4j.core.LogEventListener;
+import org.apache.logging.log4j.util.FilteredObjectInputStream;
 
 /**
  * Reads and logs serialized {@link LogEvent} objects from an {@link ObjectInputStream}.
