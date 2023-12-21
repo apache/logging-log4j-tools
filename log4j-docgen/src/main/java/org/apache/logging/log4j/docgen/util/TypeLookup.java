05a3a5d2fb0310601d0c8329a9e232dc296728e7
Piotr P. Karwasz <piotr.github@karwasz.org>
2023-12-21 15:36:08 +0100

    Cleanup `TypeLookup`

--- log4j-docgen/src/main/java/org/apache/logging/log4j/docgen/util/TypeLookup.java
+++ log4j-docgen/src/main/java/org/apache/logging/log4j/docgen/util/TypeLookup.java
@@ -17,29 +17,23 @@
 package org.apache.logging.log4j.docgen.util;
 
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
 import java.util.function.Predicate;
 import org.apache.logging.log4j.docgen.AbstractType;
 import org.apache.logging.log4j.docgen.PluginSet;
 import org.apache.logging.log4j.docgen.PluginType;
-import org.apache.logging.log4j.docgen.ScalarType;
 import org.apache.logging.log4j.docgen.Type;
 
 public class TypeLookup extends TreeMap<String, Type> {
 
     private static final Predicate<PluginType> HAS_CORE_NAMESPACE = p -> "Core".equals(p.getNamespace());
 
-    private final Map<String, PluginType> pluginTypeMap = new TreeMap<>();
-
     public static TypeLookup of(final Iterable<PluginSet> sets) {
         return new TypeLookup(sets);
     }
 
     private TypeLookup(final Iterable<PluginSet> sets) {
-        final Map<String, ScalarType> scalarTypeMap = new TreeMap<>();
-        final Map<String, AbstractType> abstractTypeMap = new TreeMap<>();
         // Round 1: Merge all the information from the sets
         sets.forEach(set -> {
             set.getScalars().forEach(scalar -> put(scalar.getClassName(), scalar));
@@ -53,12 +47,7 @@ public class TypeLookup extends TreeMap<String, Type> {
                 plugin.getSupertypes().forEach(supertype -> ((AbstractType)
                                 computeIfAbsent(supertype, TypeLookup::createAbstractType))
                         .addImplementation(plugin.getClassName()));
-                plugin.getElements().forEach(element -> {
-                    final String type = element.getType();
-                    if (!scalarTypeMap.containsKey(type) && !pluginTypeMap.containsKey(type)) {
-                        requiredAbstractTypes.add(type);
-                    }
-                });
+                plugin.getElements().forEach(element -> requiredAbstractTypes.add(element.getType()));
             });
         });
         // Round 3: remove the types that are not required and do not have a description
