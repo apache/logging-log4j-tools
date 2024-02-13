a2cde8b522059b32e8c8c863ba928401e1ecfbea
Volkan Yazıcı <volkan@yazi.ci>
2024-02-13 11:45:00 +0100

    Refactor `AsciidocConverterTest`

--- log4j-docgen/src/test/java/org/apache/logging/log4j/docgen/processor/AsciidocConverterTest.java
+++ log4j-docgen/src/test/java/org/apache/logging/log4j/docgen/processor/AsciidocConverterTest.java
@@ -21,6 +21,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.UncheckedIOException;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -29,6 +31,7 @@ import java.util.Locale;
 import java.util.Set;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
 import javax.tools.Diagnostic;
 import javax.tools.DiagnosticCollector;
 import javax.tools.DocumentationTool;
@@ -43,36 +46,52 @@ import jdk.javadoc.doclet.Doclet;
 import jdk.javadoc.doclet.DocletEnvironment;
 import jdk.javadoc.doclet.Reporter;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.CleanupMode;
+import org.junit.jupiter.api.io.TempDir;
 
 public class AsciidocConverterTest {
 
     private static final Path LICENSE_PATH = Paths.get("src/test/resources/templates/license.ftl");
 
-    @Test
-    void convertToAsciidoc() throws Exception {
-        final DocumentationTool tool = ToolProvider.getSystemDocumentationTool();
-        final DiagnosticCollector<JavaFileObject> diagnosticCollector = new DiagnosticCollector<>();
-        final StandardJavaFileManager fileManager = tool.getStandardFileManager(null, Locale.ROOT, UTF_8);
+    private static final String TEST_CLASS_NAME = "JavadocExample";
 
-        final Path sourcePath = Paths.get("src/test/resources/processor/asciidoc/example/JavadocExample.java");
+    private static final Charset CHARSET = UTF_8;
+
+    @Test
+    void convertToAsciidoc(@TempDir(cleanup = CleanupMode.ON_SUCCESS) final Path outputDir) throws Exception {
+
+        // Instantiate the tooling
+        final DocumentationTool tool = ToolProvider.getSystemDocumentationTool();
+        final StandardJavaFileManager fileManager = tool.getStandardFileManager(null, Locale.ROOT, CHARSET);
+
+        // Populate sources (aka. compilation units)
+        final Path sourcePath =
+                Paths.get("src/test/resources/AsciiDocConverterTest-input/example/" + TEST_CLASS_NAME + ".java");
         final Iterable<? extends JavaFileObject> sources = fileManager.getJavaFileObjects(sourcePath);
 
-        final Path basePath = Paths.get(System.getProperty("basedir", "."));
-        final Path destPath = basePath.resolve("target/test-site");
-        Files.createDirectories(destPath);
-        fileManager.setLocationFromPaths(StandardLocation.CLASS_OUTPUT, Set.of(destPath));
+        // Set the target path used by `Javadoc2AsciiDocDoclet` to dump the generated files
+        fileManager.setLocationFromPaths(StandardLocation.CLASS_OUTPUT, Set.of(outputDir));
 
+        // Compile the sources
+        final DiagnosticCollector<JavaFileObject> diagnosticCollector = new DiagnosticCollector<>();
         final DocumentationTask task =
-                tool.getTask(null, fileManager, diagnosticCollector, TestDoclet.class, null, sources);
+                tool.getTask(null, fileManager, diagnosticCollector, Javadoc2AsciiDocDoclet.class, null, sources);
         task.call();
 
+        // Verify successful compilation
         List<Diagnostic<? extends JavaFileObject>> diagnostics = diagnosticCollector.getDiagnostics();
         assertThat(diagnostics).noneMatch(diagnostic -> diagnostic.getKind() != Diagnostic.Kind.NOTE);
-        final Path expectedPath = Paths.get("src/test/resources/expected/processor/JavadocExample.adoc");
-        assertThat(destPath.resolve("processor/JavadocExample.adoc")).hasSameTextualContentAs(expectedPath, UTF_8);
+
+        // Verify the generated AsciiDoc
+        final Path expectedPath =
+                Paths.get("src/test/resources/AsciiDocConverterTest-reference-output/" + TEST_CLASS_NAME + ".adoc");
+        assertThat(outputDir.resolve(TEST_CLASS_NAME + ".adoc")).hasSameTextualContentAs(expectedPath, CHARSET);
     }
 
-    public static class TestDoclet implements Doclet {
+    /**
+     * A {@link Doclet} converting Javadoc to AsciiDoc only for the class called {@value #TEST_CLASS_NAME}.
+     */
+    public static final class Javadoc2AsciiDocDoclet implements Doclet {
 
         @Override
         public void init(final Locale locale, final Reporter reporter) {}
@@ -98,18 +117,20 @@ public class AsciidocConverterTest {
             final JavaFileManager fileManager = environment.getJavaFileManager();
             try {
                 for (final Element element : environment.getIncludedElements()) {
-                    if ("JavadocExample".equals(element.getSimpleName().toString())) {
+                    if (ElementKind.CLASS.equals(element.getKind())
+                            && TEST_CLASS_NAME.equals(element.getSimpleName().toString())) {
                         final FileObject output = fileManager.getFileForOutput(
-                                StandardLocation.CLASS_OUTPUT, "processor", "JavadocExample.adoc", null);
+                                StandardLocation.CLASS_OUTPUT, "", "JavadocExample.adoc", null);
                         final String asciiDoc = converter.toAsciiDoc(element);
+                        assertThat(asciiDoc).isNotNull();
                         try (final OutputStream os = output.openOutputStream()) {
                             Files.copy(LICENSE_PATH, os);
-                            os.write(asciiDoc.getBytes(UTF_8));
+                            os.write(asciiDoc.getBytes(CHARSET));
                         }
                     }
                 }
-            } catch (final IOException e) {
-                throw new RuntimeException(e);
+            } catch (final IOException error) {
+                throw new UncheckedIOException(error);
             }
             return true;
         }
